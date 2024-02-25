ddda2044e40ac0943e1cb76454308a994991de24
Ankit3060 <ankit330660@gmail.com>
2024-02-25 22:13:55 +0530

    Created Theme switcher with context API

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState,  useEffect } from 'react'
+import './App.css'
+import { ThemeProvider } from './Contexts/Theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
+
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light")
+  const darkTheme = () => setThemeMode("dark")
+  const lightTheme = () => setThemeMode("light")
+
+  //Actual change in theme
+  useEffect(() => {
+    document.querySelector('html').classList.remove('dark','light')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
+
+  return (
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* Themebtn */}
+          <ThemeBtn />
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+          {/* Card */}
+          <Card />
+        </div>
+      </div>
+    </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
